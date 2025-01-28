571d9267dc501b6589987c674ef669408c983607
AFKJonas <jonasengelsgaard@gmail.com>
2025-01-28 21:13:04 +0100

    Fikset/adda kommentarer
    La til kommentarer og plasserte showInfoKort('HTML') øverst

--- Arbeidskrav_1/arbeidskravScript.js
+++ Arbeidskrav_1/arbeidskravScript.js
@@ -1,23 +1,30 @@
+ //Er øverst i scriptet for at infokortet med 'HTML' skal lastes/kjøre først, (kaller funksjonen for å vise HTML-informasjonen når siden lastes):
+ showInfoKort('HTML');
 //loggfører til konsollen for å sjekke at arrayet er korrekt:
 console.log(resources);
 
 //laget en variabel som skal holde på HTML-koden som skal vises på nettsiden:
 let resourcesHTML = "";
 
-//lager en .map() for å hente ut data fra arrayet og legge det inn i HTML-koden:
+//lager en .map() for å hente ut data fra arrayet(resources) og legge det inn i HTML-koden:
 resources.map((resource, index) => resourcesHTML += `<article>
         <h2>${resource.category}</h2>
         <p>${resource.text}</p>
         <nav>
             <ul>
+            <!--lager en .map() for å hente ut data fra arrayet "sources" på innsiden av arrayet "resources", og legger dataen(title og url) fra sources inn i HTML-koden:-->
                 ${resource.sources.map((source, index) => `<li><a href="${source.url}">${source.title}</a></li>`).join('')}
             </ul>
         </nav>
     </article>`);
     
+    //funksjon her for filtrere den infoen til den kategorien som er valgt og viser den.   
     function showInfoKort(category) {
+    //const her for å hente ut elementet med id "infoSide" fra HTML-filen: 
         const infoSide = document.getElementById("infoSide");
+    //bruker .filter() for å filtrere ut data fra arrayet(resources) basert på hvilken kategori som er valgt:
         const filteredResources = resources.filter(resource => resource.category === category);
+    //bruker .map() her for å hente ut dataen fra filteredResources og legge den inn i HTML-koden:
         const filteredHTML = filteredResources.map(resource => `
             <article>
                 <h2>${resource.category}</h2>
@@ -30,6 +37,7 @@ resources.map((resource, index) => resourcesHTML += `<article>
             </article>
         `).join('');
     
+        //Plasserer infoen inn i der id="infoSide"/ er lokalisert i HTML-filen:
         infoSide.innerHTML = filteredHTML;  
     }
 
@@ -41,12 +49,12 @@ resources.map((resource, index) => resourcesHTML += `<article>
         document.getElementById("reactKnapp").style.backgroundColor = "#fff";
         document.getElementById("sanityKnapp").style.backgroundColor = "#fff";
     }
+   
     
-    //addEventListener med "click" for å kalle funksjonen showInfoKort() og dens innhold: 
+    //addEventListener med "click" for å kalle på funksjonen showInfoKort() og dens innhold: 
     document.getElementById("htmlKnapp").addEventListener("click", () => showInfoKort('HTML'));
     document.getElementById("cssKnapp").addEventListener("click", () => showInfoKort('CSS'));
     document.getElementById("jsKnapp").addEventListener("click", () => showInfoKort('JavaScript'));
     document.getElementById("reactKnapp").addEventListener("click", () => showInfoKort('React'));
     document.getElementById("sanityKnapp").addEventListener("click", () => showInfoKort('Sanity and headless CMS'));
-    //kaller funksjonen for å vise HTML-informasjonen når siden lastes:
-    showInfoKort('HTML');
+   
